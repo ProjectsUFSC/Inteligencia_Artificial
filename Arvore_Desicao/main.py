bf3fe9771ebdc625a7af6cc203e7429fdf08d238
Daleffe <augustodaleffe@gmail.com>
2024-10-29 18:55:16 -0300

    aula de desição aula 10/29

---
+++ Arvore_Desicao/main.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn import tree
+import matplotlib.pyplot as plt
+
+dados = pd.read_excel("./data.xlsx")
+
+print(dados) # print da tabela
+
+lb_encoder = LabelEncoder()
+
+
+dados['Tempo'] = lb_encoder.fit_transform(dados['Tempo'])
+dados['Vento'] = lb_encoder.fit_transform(dados['Vento'])
+dados['Joga (sim/nao)'] = lb_encoder.fit_transform(dados['Joga (sim/nao)'])
+
+
+print("\n\n\n\n\n")
+
+print(dados) # vendo a mudança que o encoder fez
+
+arvore_decisao = tree.DecisionTreeClassifier()
+
+x = dados.iloc[:,0:4]
+
+print("\n\n\n\n\n")
+print(x) # ignorando o Y e mantendo só o X
+
+y = dados.iloc[:,-1]
+
+print("\n\n\n\n\n")
+print(y) # mantendo só o Y
+
+arvore_decisao = arvore_decisao.fit(x,y)
+
+print("\n\n\n\n\n")
+tree.plot_tree(arvore_decisao, feature_names=x.columns, class_names=['Sim', 'Não'], filled=True) # plotando a arvore de decisão
+plt.show()
